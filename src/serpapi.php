3cfa28456beefda052685f834b2a63a36a7635e7
Igor Galeta <igor.galeta@serpapi.com>
2026-06-30 10:53:43 +0300

    Fix invalid PHPDoc annotations

--- src/serpapi.php
+++ src/serpapi.php
@@ -1,9 +1,17 @@
 <?php
 
 class SerpApi {
+  /** @var string */
   public $api_key;
+
+  /** @var string */
   public $engine;
 
+  /**
+   * @param string $api_key
+   * @param string $engine
+   * @throws SerpApiException
+   */
   function __construct($api_key = '', $engine = 'google') {
     if(empty($engine)) {
       throw new SerpApiException("engine must be present");
@@ -13,39 +21,54 @@ class SerpApi {
     $this->engine = $engine;
   }
 
-  /***
-   * search
-   * @return [Hash] search result "json like"
+  /**
+   * Run a search and return decoded JSON.
+   *
+   * @param array<string, mixed> $params
+   * @return object
    */
   public function search($params = []) {
     return $this->get('/search', 'json', $params);
   }
 
-  /***
-   * html
-   * @return [String] raw html search result
+  /**
+   * Run a search and return raw HTML.
+   *
+   * @param array<string, mixed> $params
+   * @return string
    */
   public function html($params = []) {
     return $this->get('/search', 'html', $params);
   }
 
- /***
-  * Get account information using Account API
-  */
+  /**
+   * Get account information using Account API.
+   *
+   * @param string|null $api_key
+   * @return object
+   */
   public function account($api_key = null) {
     $params = empty($api_key) ? [] : ['api_key' => $api_key];
     return $this->get('/account', 'json', $params);
   }
 
-  /***
-   * Get location using Location API
+  /**
+   * Get locations using Location API.
+   *
+   * @param array<string, mixed> $params
+   * @return array<int, object>
    */
   public function location($params = []) {
     return $this->get("/locations.json", 'json', $params);
   }
 
-  /***
-   * Retrieve search result from the Search Archive API
+  /**
+   * Retrieve search result from the Search Archive API.
+   *
+   * @param string|null $search_id
+   * @param string $format
+   * @return object|string
+   * @throws SerpApiException
    */
   public function search_archive($search_id = null, $format = 'json') {
     if(empty($search_id)) {
@@ -60,6 +83,13 @@ class SerpApi {
     return $this->get("/searches/{$safe_search_id}.{$format}", $format, []);
   }
 
+  /**
+   * @param string|null $endpoint
+   * @param string $format
+   * @param array<string, mixed> $params
+   * @return object|string
+   * @throws SerpApiException
+   */
   private function get($endpoint = null, $format = 'json', $params = []) {
     if(!in_array($format, ['json', 'html'], true)) {
       throw new SerpApiException("Unsupported format '$format'. Expected 'html' or 'json'.");
@@ -112,6 +142,11 @@ class SerpApiSearch extends SerpApi {
     parent::__construct($api_key, $engine);
   }
 
+  /**
+   * @param string $api_key
+   * @return void
+   * @throws SerpApiException
+   */
   function set_serp_api_key($api_key) {
     if(empty($api_key)) {
       throw new SerpApiException("serp_api_key must have a value");
@@ -120,14 +155,28 @@ class SerpApiSearch extends SerpApi {
     $this->api_key = $api_key;
   }
 
+  /**
+   * @param array<string, mixed> $params
+   * @return object
+   */
   function get_json($params = []) {
     return parent::search($params);
   }
 
+  /**
+   * @param array<string, mixed> $params
+   * @return string
+   */
   function get_html($params = []) {
     return parent::html($params);
   }
 
+  /**
+   * @param string|array<string, mixed>|null $output
+   * @param array<string, mixed> $params
+   * @return object|string
+   * @throws SerpApiException
+   */
   function search($output = null, $params = []) {
     if($output === null || is_array($output)) {
       return parent::search(is_array($output) ? $output : $params);
@@ -142,6 +191,12 @@ class SerpApiSearch extends SerpApi {
     }
   }
 
+  /**
+   * @param string|null $location
+   * @param int $limit
+   * @return array<int, object>
+   * @throws SerpApiException
+   */
   function get_location($location = null, $limit = 3) {
     if(empty($location)) {
       throw new SerpApiException("location must be present");
@@ -159,12 +214,18 @@ class SerpApiSearch extends SerpApi {
     return parent::location($params);
   }
 
+  /**
+   * @return object
+   */
   function get_account() {
     return parent::account($this->api_key);
   }
 }
 
 class GoogleSearch extends SerpApiSearch {
+  /**
+   * @param string $api_key
+   */
   public function __construct($api_key) {
     parent::__construct($api_key, 'google');
   }
